5f4be3beb873cc0b98aba0217ca44f97d3a50af0
etamin-code <70692373+etamin-code@users.noreply.github.com>
2022-06-04 19:09:21 +0300

    Update run_nodes.sh

--- hw_2/run_nodes.sh
+++ hw_2/run_nodes.sh
@@ -6,21 +6,18 @@ sudo docker run -d\
     -it \
     --network hazelcast-network \
     --rm \
-    -e HZ_CLUSTERNAME=my_cluster \
     -p 5701:5701 hazelcast/hazelcast:5.1.1
     
 sudo docker run -d\
     -it \
     --network hazelcast-network \
     --rm \
-    -e HZ_CLUSTERNAME=my_cluster \
     -p 5702:5701 hazelcast/hazelcast:5.1.1
 
 sudo docker run -d \
     -it \
     --network hazelcast-network \
     --rm \
-    -e HZ_CLUSTERNAME=my_cluster \
     -p 5703:5701 hazelcast/hazelcast:5.1.1
     
 sudo docker run -d \
