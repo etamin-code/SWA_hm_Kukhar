713e356b87d2cf8bc11db46cb59e77f1b6e90544
etamin-code <70692373+etamin-code@users.noreply.github.com>
2022-06-04 19:12:10 +0300

    Update create_map.py

--- hw_2/task_3/create_map.py
+++ hw_2/task_3/create_map.py
@@ -1,17 +1,12 @@
 import hazelcast
 
 
-client = hazelcast.HazelcastClient(cluster_name="dev",
-                                   cluster_members=[
-                                       "172.18.0.2:5701",
-                                       "172.18.0.3:5701",
-                                       "172.18.0.4:5701"
-                                   ])
+client = hazelcast.HazelcastClient(cluster_name="dev")
                                    
 my_map = client.get_map("my-distributed-map").blocking()
 
-for idx in range(1000):
-    my_map.put(f"key-{idx}", f"value-{idx}")
+for i in range(1000):
+    my_map.put(f"key-{i}", f"value-{i}")
 client.shutdown()
 print("done")
 
